e58ecb7a9d3d71af1072a16af03fd6c035b2d0b5
Ariel Rivero <ariel200506@gmail.com>
2022-04-12 02:41:24 -0300

    Controller para el formulario de cliente

--- src/main/java/com/sistemadefacturacion/springboot/app/models/dao/ClienteDaoImp.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/models/dao/ClienteDaoImp.java
@@ -23,4 +23,10 @@ public class ClienteDaoImp implements IClienteDao {
 		return em.createQuery("from Cliente").getResultList();
 	}
 
+	@Override
+	@Transactional
+	public void save(Cliente cliente) {
+		em.persist(cliente);
+	}
+
 }
