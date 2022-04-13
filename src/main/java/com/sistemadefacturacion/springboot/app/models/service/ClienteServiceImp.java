532f2c6ce59444210c2d77543edd650c7885aa35
Ariel Rivero <ariel200506@gmail.com>
2022-04-13 02:16:35 -0300

    Agrego crudRepository y quito la implementación de la interface

--- src/main/java/com/sistemadefacturacion/springboot/app/models/service/ClienteServiceImp.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/models/service/ClienteServiceImp.java
@@ -3,7 +3,6 @@ package com.sistemadefacturacion.springboot.app.models.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +13,12 @@ import com.sistemadefacturacion.springboot.app.models.entity.Cliente;
 public class ClienteServiceImp implements IClienteService{
 	
 	@Autowired
-	@Qualifier("clienteDaoJPA")
 	private IClienteDao clienteDao;
 	
 	@Override
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
-		return clienteDao.findAll();
+		return (List<Cliente>) clienteDao.findAll();
 	}
 
 	@Override
@@ -32,13 +30,13 @@ public class ClienteServiceImp implements IClienteService{
 	@Override
 	@Transactional(readOnly = true)
 	public Cliente findOne(Long id) {
-		return clienteDao.findOne(id);
+		return clienteDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Long id) {
-		clienteDao.delete(id);
+		clienteDao.deleteById(id);
 	}
 
 }
