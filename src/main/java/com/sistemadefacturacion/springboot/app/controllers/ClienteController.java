4e61eca5b528b4999093c88e842f4ca6de5ec433
Ariel Rivero <ariel200506@gmail.com>
2022-04-13 01:49:44 -0300

    Agrego mensajes personalizado y guardo en objeto cliente en session del controller

--- src/main/java/com/sistemadefacturacion/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/controllers/ClienteController.java
@@ -9,13 +9,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.sistemadefacturacion.springboot.app.models.dao.IClienteDao;
 import com.sistemadefacturacion.springboot.app.models.entity.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 
 	@Autowired
@@ -38,7 +42,7 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de clientes");
@@ -46,7 +50,23 @@ public class ClienteController {
 		}
 		
 		clienteDao.save(cliente);
-		
+		status.setComplete();
 		return "redirect:listar";
+	}
+	
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+		Cliente cliente = null;
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);
+		}
+		else {
+			return "redirect:/listar";
+		}
+		
+		model.put("titulo", "Editar cliente");
+		model.put("cliente", cliente);
+		
+		return "form";
 	}
 }
