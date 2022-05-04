fb6af3241e08170b1b1de3f4c2839edf63bcf1d1
Ariel Rivero <ariel200506@gmail.com>
2022-05-04 02:47:26 -0300

    Clases itemFactura y factura

--- src/main/java/com/sistemadefacturacion/springboot/app/models/entity/Factura.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.sistemadefacturacion.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -34,9 +39,17 @@ public class Factura implements Serializable{
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name="factura_id")
+	private List<ItemFactura> item;
+	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
+	}
+	
+	public Factura() {
+		this.item = new ArrayList<ItemFactura>();
 	}
 	
 	public Long getId() {
@@ -68,5 +81,17 @@ public class Factura implements Serializable{
 	}
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItem() {
+		return item;
+	}
+
+	public void setItem(List<ItemFactura> item) {
+		this.item = item;
+	}
+	
+	public void addItemFactura(ItemFactura item) {
+		this.item.add(item);
 	}
 }
