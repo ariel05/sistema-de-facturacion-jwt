e40f5815719fdbe50de041ed62295a8425bead41
Ariel Rivero <ariel200506@gmail.com>
2022-05-04 02:27:46 -0300

    Agrego carga de imagen por servicio

--- src/main/java/com/sistemadefacturacion/springboot/app/SistemaDeFacturacionApplication.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/SistemaDeFacturacionApplication.java
@@ -1,13 +1,27 @@
 package com.sistemadefacturacion.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaDeFacturacionApplication {
+import com.sistemadefacturacion.springboot.app.models.service.IUploadFileService;
 
+@SpringBootApplication
+public class SistemaDeFacturacionApplication implements CommandLineRunner{
+
+	@Autowired
+	private IUploadFileService uploadFileService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDeFacturacionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.uploadFileService.deleteAll();
+		this.uploadFileService.init();
+		
 	}
 
 }
--- src/main/java/com/sistemadefacturacion/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/controllers/ClienteController.java
@@ -1,21 +1,31 @@
 package com.sistemadefacturacion.springboot.app.controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +37,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sistemadefacturacion.springboot.app.models.entity.Cliente;
 import com.sistemadefacturacion.springboot.app.models.service.IClienteService;
+import com.sistemadefacturacion.springboot.app.models.service.IUploadFileService;
 import com.sistemadefacturacion.springboot.app.util.paginator.PageRender;
 
 @Controller
@@ -36,6 +47,38 @@ public class ClienteController {
 	@Autowired
 	@Qualifier("clienteService")
 	private IClienteService clienteService;
+
+	@Autowired
+	private IUploadFileService uploadFileService;
+	
+	@GetMapping(value="/uploads/{filename:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
+		Resource recurso = null;
+		try {
+			recurso = uploadFileService.load(filename);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"")
+				.body(recurso);
+
+	}
+	@GetMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente = clienteService.findOne(id);
+		if(cliente == null){
+			flash.addFlashAttribute("error", "El cliente no existe");
+			return "redirect:/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle de cliente: " + cliente.getNombre().concat(" ").concat(cliente.getApellido()));
+		
+		return "ver";
+	}
 	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(@RequestParam(name="page", defaultValue = "0") int page, Model model) {
@@ -67,21 +110,22 @@ public class ClienteController {
 		}
 		
 		if(!foto.isEmpty()) {
-			Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
-			String rootPath = directorioRecursos.toFile().getAbsolutePath();
 			
+			if(cliente.getId() != null && cliente.getId() > 0 && cliente.getFoto() != null && cliente.getFoto().length() > 0) {
+				uploadFileService.delete(cliente.getFoto());
+			}
+			
+			String uniqueFileName = null;
 			try {
-				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
-				
-				flash.addFlashAttribute("info", "Has subido correctamente '" + foto.getOriginalFilename() + "'");
-				
-				cliente.setFoto(foto.getOriginalFilename());
+				uniqueFileName = uploadFileService.copy(foto);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFileName + "'");
+			
+			cliente.setFoto(uniqueFileName);
+			
 		}
 		
 		String mensajeFlash = cliente.getId() != null ? "Cliente editado con éxito" : "Cliente agregado con éxito";
@@ -117,8 +161,14 @@ public class ClienteController {
 	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if(id > 0) {
+			Cliente cliente = clienteService.findOne(id);
+			
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
+			
+			if(uploadFileService.delete(cliente.getFoto())) {
+				flash.addAttribute("info", "Foto "+ cliente.getFoto() + " eliminada con éxito!");
+			}
 		}
 		return "redirect:/listar";
 	}
