ff8a138cabbbe4f42396bbcac56de3a28ee32679
Ariel Rivero <ariel200506@gmail.com>
2022-05-25 03:03:53 -0300

    Agrego autenticación con JPA

--- src/main/java/com/sistemadefacturacion/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/SpringSecurityConfig.java
@@ -6,12 +6,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.sistemadefacturacion.springboot.app.auth.handler.LoginSuccessHandler;
+import com.sistemadefacturacion.springboot.app.models.service.JpaUserDetailsService;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
@@ -24,16 +22,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
+	private JpaUserDetailsService userDetailService;
+	
+	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
+
+		builder.userDetailsService(userDetailService)
+		.passwordEncoder(passwordEncoder);
+
 		
-		PasswordEncoder encoder = this.passwordEncoder;
+//		builder.jdbcAuthentication().dataSource(datasource)
+//		.passwordEncoder(passwordEncoder)
+//		.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+//		.authoritiesByUsernameQuery("SELECT u.username, a.authority FROM authorities a JOIN users u ON (a.user_id=u.id) WHERE u.username=?");
 		
-//		UserBuilder users = User.builder().passwordEncoder(password ->  encoder.encode(password));
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-		
-		builder.inMemoryAuthentication()
-		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-		.withUser(users.username("ariel").password("12345").roles("USER"));
+//		PasswordEncoder encoder = this.passwordEncoder;
+//		
+////		UserBuilder users = User.builder().passwordEncoder(password ->  encoder.encode(password));
+//		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//		
+//		builder.inMemoryAuthentication()
+//		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+//		.withUser(users.username("ariel").password("12345").roles("USER"));
 	}
 
 	@Override
