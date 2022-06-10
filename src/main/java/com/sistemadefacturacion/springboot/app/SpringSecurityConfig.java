35c7f1db9262754f8a1a176ae54be57f96465945
Ariel Rivero <ariel200506@gmail.com>
2022-06-09 23:25:55 -0300

    Agrego configuración para trabajar con JWT y no con sessiones

--- src/main/java/com/sistemadefacturacion/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.sistemadefacturacion.springboot.app.auth.handler.LoginSuccessHandler;
@@ -65,7 +66,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 			.permitAll()
 		.and()
 			.exceptionHandling()
-			.accessDeniedPage("/error_403");
+			.accessDeniedPage("/error_403")
+		.and()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 	
