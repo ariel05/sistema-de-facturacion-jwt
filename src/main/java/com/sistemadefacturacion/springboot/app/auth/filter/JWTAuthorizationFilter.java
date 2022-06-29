70b64cb3b4fcaf1ef0c111fd6b60a7139360c1d3
Ariel Rivero <ariel200506@gmail.com>
2022-06-29 01:24:01 -0300

    Valido token con parse

--- src/main/java/com/sistemadefacturacion/springboot/app/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
 	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
@@ -19,6 +23,26 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		String header = request.getHeader("Authorization");
+		
+		if(!requiresAuthentication(header)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		boolean tokenValido;
+		Claims token = null;
+		
+		try {
+			token = Jwts.parserBuilder()
+					.setSigningKey("Alguna.Super.Clave.Secreta.jajaja".getBytes())
+					.build()
+					.parseClaimsJws(header.replace("Bearer ", ""))
+					.getBody();
+			tokenValido = true;
+		}catch(JwtException | IllegalArgumentException e){
+			tokenValido = false;
+		}
 	}
 	
 	protected boolean requiresAuthentication(String header) {
