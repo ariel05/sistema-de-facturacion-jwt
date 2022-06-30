e8cf382a38d9d689c84a1bcc1a0f05283780d14a
Ariel Rivero <ariel200506@gmail.com>
2022-06-30 00:10:37 -0300

    Agrego autenticacion con el token enviado por el cliente

--- src/main/java/com/sistemadefacturacion/springboot/app/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/sistemadefacturacion/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package com.sistemadefacturacion.springboot.app.auth.filter;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,11 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.lang.Arrays;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
@@ -43,6 +52,20 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 		}catch(JwtException | IllegalArgumentException e){
 			tokenValido = false;
 		}
+		
+		UsernamePasswordAuthenticationToken authentication = null;
+		
+		if(tokenValido) {
+			String username = token.getSubject();
+			Object roles = token.get("Authority");
+			
+			Collection<? extends GrantedAuthority> authorities = java.util.Arrays.asList(new ObjectMapper().readValue(roles.toString(), SimpleGrantedAuthority[].class));
+			
+			authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+		}
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
 	}
 	
 	protected boolean requiresAuthentication(String header) {
